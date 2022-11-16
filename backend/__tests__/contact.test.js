362c2494353451fdf83822a5edc7002edbcf97ed
Yasser <yassergeo@gmail.com>
2022-11-16 11:15:48 -0500

    added create tests in to controller

--- backend/__tests__/contact.test.js
+++ backend/__tests__/contact.test.js
@@ -1,6 +1,6 @@
 const mockingoose = require('mockingoose')
 const ContactModel = require('../models/Contact')
-const {findAllContacts} = require('../controllers/contact')
+const {findAllContacts, createAContact} = require('../controllers/contact')
 
   describe('Test contact controllers', () => {
     beforeEach(() => {
@@ -29,9 +29,35 @@ const {findAllContacts} = require('../controllers/contact')
           ]
         mockingoose(ContactModel).toReturn(response, 'find');
 
-          const result = await findAllContacts()
+        const result = await findAllContacts()
 
-          expect(JSON.parse(JSON.stringify(result))).not.toBe([])
-          expect(JSON.parse(JSON.stringify(result))).toMatchObject(response)
+        expect(JSON.parse(JSON.stringify(result))).not.toBe([])
+        expect(JSON.parse(JSON.stringify(result))).not.toBeUndefined()
+        expect(JSON.parse(JSON.stringify(result))).not.toBeNull()
+        expect(JSON.parse(JSON.stringify(result))).toMatchObject(response)
+    })
+    it('should create a contact', async () => {
+        const data = {
+            name: "yasserX",
+            address: "Pelieo",
+            phoneNumber: "99780",
+            email: "yasserxx@hotmail.com",
+          }
+
+        const response = {
+              name: "yasserX",
+              address: "Pelieo",
+              phoneNumber: "99780",
+              email: "yasserxx@hotmail.com",
+            }
+          
+        mockingoose(ContactModel).toReturn(response, 'save');
+
+        const result = await createAContact(data)
+
+        expect(JSON.parse(JSON.stringify(result))).not.toBe([])
+        expect(JSON.parse(JSON.stringify(result))).not.toBeUndefined()
+        expect(JSON.parse(JSON.stringify(result))).not.toBeNull()
+        expect(JSON.parse(JSON.stringify(result))).toMatchObject(response)
     })
   })
