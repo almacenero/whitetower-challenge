094b0b35923821e24ea51d48c37640e6f014addd
Yasser <yassergeo@gmail.com>
2022-11-15 20:49:57 -0500

    added get a contact by id route

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -4,30 +4,43 @@ const Contact = require('../models/Contact')
 const router = Router()
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try{
+    const response = await Contact.find()
+    res.send(response) 
+  }catch(error) {
+    console.log('Error ', error)
+    res.json({message: error})
+  }
   res.send('welcome white tower clients')
 })
 
 router.post('/', async (req, res) => {
-  try {
     const contact =  new Contact({
       name: req.body.name,
       address: req.body.address,
       phoneNumber: req.body.phoneNumber,
       email:  req.body.email
     })
-    console.log('contact--->', contact)
+    try {
     const response = await contact.save()
-    console.log("response --->", response)
-    res.send('creeated')
-    
-    
+    res.send(response) 
   
   } catch(error) {
     console.log('Error ', error)
     res.json({message: error})
   }
   
+})
+
+router.get('/:id', async (req, res) => {
+  try{
+    const response = await Contact.findById(req.params.id)
+    res.send(response) 
+  }catch(error) {
+    console.log('Error ', error)
+    res.json({message: error})
+  }
 })
 
 module.exports = router
