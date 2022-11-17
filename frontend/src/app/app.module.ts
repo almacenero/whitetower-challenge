90d54c2efd99c0348c64598a5c00ea2e3301615d
Yasser <yassergeo@gmail.com>
2022-11-17 17:58:08 -0500

    added layout to contacts component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactComponent } from './contacts/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    ContactsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
